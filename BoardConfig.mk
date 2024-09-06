76b4706356376846fae591dd92e298e54f439f2e
SzuWei Lin <szuweilin@google.com>
2024-09-06 23:50:59 +0100

    air: Support dynamic 64-bit drmserver
    64-bit GSI requires support both 64_32-bit and 64-bit devices at
    the same time. Add a new variable TARGET_DYNAMIC_64_32_DRMSERVER
    to put drmserver_dynamic.rc into GSI. Which starts 64-bit
    drmserver if ro.zygote is zygote64. Otherwise, it keeps the
    original behavior to start 32-bit drmserver.
    
    Bug: 282603373
    Test: make gsi_arm64-user; Check system/etc/init
    Test: make gsi_arm-user; Check systen/etc/init
    Change-Id: I874cee00a066086f565e044549ad40c6ab5d62b0
    Merged-In: I874cee00a066086f565e044549ad40c6ab5d62b0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,9 @@ KERNEL_PATH := device/xiaomi/air-kernel
 # Enable 64-bit for non-zygote.
 ZYGOTE_FORCE_64 := true
 
+# Include 64-bit drmserver to support 64-bit only devices
+TARGET_DYNAMIC_64_32_DRMSERVER := true
+
 # A/B
 AB_OTA_PARTITIONS := \
     boot \
